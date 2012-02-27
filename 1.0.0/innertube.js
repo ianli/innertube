68c0c23528e949007b7ee254a3853a13fc5b3c2e
Ian Li <ianarli@gmail.com>
2012-02-27 03:46:01 -0500

    Cleaned up Innertube 1.0.0

--- 1.0.0/innertube.js
+++ 1.0.0/innertube.js
@@ -14,257 +14,8 @@
 // The function sandboxes the code.
 // Pass local reference to window for performance reasons.
 // Redefines undefined as it could have been tampered with.
-;(function (window, _, $, undefined) {
+;(function (window, _, easyXDM, undefined) {
   
-  // Innertube.RPC
-  // ===============================================
-  // Innertube.RPC is the interface between widgets and the dashboard.
-  // `localProperties` include methods that are called by the local RPC instance;
-  // they cannot be called by remote entities.
-  // On the other hand, `remoteProperties` are methods that are called by remote entities.
-  var RPC = function (localProperties, remoteProperties) {
-  	// We use the variable self to represent this object
-  	// and to reduce confusion regarding what `this` refers to.
-  	var self = this,
-
-  	// Make copies of the arguments, because we'll be modifying them.
-  	localProps = _.clone(localProperties),
-  	remoteProps = _.clone(remoteProperties);
-
-
-  	// Assign default values to the local properties
-  	// if left unassigned.
-  	_.defaults(localProps, {
-  		// Called when easyXDM.Rpc is ready.
-  		// If left unassigned, do nothing.
-  		ready: function() {},
-
-  		// Called when a method doesn't exist.
-  		// If left unassigned, do nothing.
-  		methodMissing: function() {}
-  	});
-
-  	// Current version of the Innertube.RPC
-  	RPC.VERSION = '0.1.0';
-
-  	// Add a remote method that returns the version number of this RPC.
-  	// We assign it here, so that it cannot be overridden.
-  	remoteProps.version = function () {
-  		return RPC.VERSION;
-  	};
-
-  	// We use easyXDM.Rpc as a way to communicate
-  	// between the widget and the dashboard.
-  	var rpc = new easyXDM.Rpc({
-  			// This function is called when the RPC is ready.
-  			onReady: function() {
-  				if (_.isFunction(localProps.ready)) {
-  					localProps.ready.apply(self);
-  				}
-  			}
-  		},
-  		// Interface Configuration
-  		{
-  			local: {
-  				widget: function(method, args, successFn, errorFn) {												
-  					if (_.isFunction(remoteProps[method])) {
-  						var returnValue = remoteProps[method].apply(self, args);
-
-  						if (!_.isUndefined(returnValue)) {
-  							return returnValue;
-  						}
-  					} else {
-  						// No method exists, call the `methodMissing` method.
-  						if (_.isFunction(localProps.methodMissing)) {
-  							localProps.methodMissing.apply(self);
-  						} else {
-  							// Otherwise, fail silently.
-  						}
-  					}
-  				}
-  			},
-  			remote: {
-  				// Invoke methods on the dashboard.
-  				dashboard: {}
-  			}
-  		}
-  	);
-
-  	// Sends messages to the Innertube Dashboard.
-  	// `method` is the method to call on the Innertube Dashboard.
-  	// The remaining arguments are passed as arguments to the method
-  	// in the Innertube Dashboard.
-  	//
-  	// There are multiple approaches to call methods on the dashboard.
-  	// 1. Methods with no arguments.
-  	// 		dashboard(method);
-  	//
-  	// 2. Methods with arguments.
-  	//		dashboard(method, arg1, arg2, arg3, ...)
-  	//
-  	// 3. Methods with no arguments and returns a value.
-  	//		dashboard(method, {
-  	//			success: function(returnValue) {
-  	//				...
-  	//			}
-  	//		});
-  	//
-  	// 4. Methods with arguments and returns a value.
-  	//		dashboard(method, arg1, arg2, ..., {
-  	//			success: function(returnValue) {
-  	//				...
-  	//			}	
-  	//		});
-  	self.dashboard = function (method) {
-  		var args = Array.prototype.slice.call(arguments, 1);
-  		if (args.length == 0) {
-  			// Approach #1
-  			// Must send an empty array as argument
-  			// to match signature of dashboard(method, args)
-  			rpc.dashboard(method, []);
-  		} else {
-  			// Check if the last argument is an object
-  			// `success` and `error` functions.
-  			var lastArg = args[args.length - 1];
-  			if (lastArg.success || lastArg.error) {
-  				// Remove the last element of the arguments list.
-  				args.pop();
-
-  				var success = lastArg.success || function () {};
-  				var error = lastArg.error || function() {};
-
-  				// Approach #3 and #4
-  				rpc.dashboard(method, args, success, error);
-  			} else {
-  				// Approach #2
-  				rpc.dashboard(method, args);	
-  			}
-  		}
-
-  		return self;
-  	};
-  };
-
-  var RPC2 = function (options) {
-    // We use the variable self to represent this object
-  	// and to reduce confusion regarding what `this` refers to.
-    var self = this
-    
-      // Whether the RPC is ready.
-      , isReady = false
-    
-      // Functions called when the RPC is ready.
-      , readyCallbacks = []
-    
-      // A hash of functions when a message is received from the dashboard.
-      , receiveCallbacks = {};
-      
-    if (typeof options === 'undefined' || options === null) {
-      options = {};
-    }
-    
-    _.defaults(options, {
-      remote: null,
-      container: null
-    });
-    
-    var rpc = new RPC(
-  		// Local methods
-  		{
-  		  remote: options['remote'],
-  		  
-  		  container: options.container,
-  		  
-  			// When the RPC is ready, set the height of the widget
-  			// and get the current date to visualize.
-  			ready: function() {
-  			  isReady = true;
-  			  for (var i = 0, n = readyCallbacks.length; i < n; i++) {
-  			    readyCallbacks[i].apply(self);
-  			  }
-  			}
-  		},
-  		// Remote methods callable by the dashboard.
-  		{
-  			// Handles when the dashboard calls `date`.
-  			date: function (range, year, month, day) {
-  			  handle('date', {
-            range: range,
-            year: year, 
-            month: month, 
-            day: day     		    
-  			  });
-  			},
-  			
-  			height: function (height) {
-  			  handle('height', height);
-  			},
-
-  			highlight: function (value) {
-  			  handle('highlight', value);
-  			}
-  		}
-  	);
-  	
-  	var handle = function(methodName) {
-  	  var args = Array.prototype.slice.call(arguments, 1);
-  	  if (_.has(receiveCallbacks, methodName)) {
-  	    var callbacks = receiveCallbacks[methodName];
-			  for (var i = 0, n = callbacks.length; i < n; i++) {
-			    callbacks[i].apply(self, args);
-			  }
-  	  }
-  	};
-  	
-  	
-  	// ======
-  	// PUBLIC
-  	// ======
-  	
-  	self.ready = function(callback) {
-  	  if (_.isFunction(callback)) {
-  	    if (isReady) {
-  	      callback.apply(self);
-  	    } else {
-  	      readyCallbacks.push(callback);
-  	    }
-  	  }
-  	  return self;
-  	};
-  	
-  	self.send = function (methodName) {
-  	  rpc.dashboard.apply(rpc, arguments);
-  	  return self;
-  	};
-  	
-  	self.request = function (methodName) {
-  	  var args = Array.prototype.slice.call(arguments, 0);
-  	  args.push({
-  	    success: function () {
-  	      var args = Array.prototype.slice.call(arguments, 0);
-  	      args = [methodName].concat(args);
-  	      handle.apply(self, args);
-  	    }
-  	  });
-  	  rpc.dashboard.apply(rpc, args);
-  	  return self;
-  	};
-  	
-  	self.receive = function (methodName, callback) {
-  	  if (!_.isFunction(callback))
-  	    return self;
-  	  
-  	  if (_.has(receiveCallbacks, methodName)) {
-  	    receiveCallbacks[methodName].push(callback);
-  	  } else {
-  	    receiveCallbacks[methodName] = [callback];
-  	  }
-  	  
-  	  return self;
-  	};
-	};
-	
-	
 	var Messaging = function (options) {
     // We use the variable self to represent this object
   	// and to reduce confusion regarding what `this` refers to.
@@ -279,57 +30,22 @@
       // Functions called when a method is missing.
       , methodMissingCallbacks = []
     
-      // A hash of functions when a message is received from the dashboard.
+      // A hash of functions when a message is received.
       , receiveCallbacks = {};
       
     if (typeof options === 'undefined' || options === null) {
+      // There were no options passed or it was null, so assign it a value.
       options = {};
     } else {
-      options = _.clone(options)
+      // Options were passed.
+      // Clone it, so we can change it without affecting the passed object.
+      options = _.clone(options);
     }
     
     _.defaults(options, {
       remote: null,
       container: null
     });
-    
-    //     var rpc = new RPC(
-    //  // Local methods
-    //  {
-    //    remote: options.remote,
-    //    
-    //    container: options.container,
-    //    
-    //    // When the RPC is ready, set the height of the widget
-    //    // and get the current date to visualize.
-    //    ready: function() {
-    //      isReady = true;
-    //      for (var i = 0, n = readyCallbacks.length; i < n; i++) {
-    //        readyCallbacks[i].apply(self);
-    //      }
-    //    }
-    //  },
-    //  // Remote methods callable by the dashboard.
-    //  {
-    //    // Handles when the dashboard calls `date`.
-    //    date: function (range, year, month, day) {
-    //      handle('date', {
-    //             range: range,
-    //             year: year, 
-    //             month: month, 
-    //             day: day             
-    //      });
-    //    },
-    //    
-    //    height: function (height) {
-    //      handle('height', height);
-    //    },
-    // 
-    //    highlight: function (value) {
-    //      handle('highlight', value);
-    //    }
-    //  }
-    // );
     
     
     // ===========
@@ -362,24 +78,12 @@
   		// easyXDM.Rpc Interface Configuration
   		{
   		  local: {
-  		    dashboard: function (method, args, successFn, errorFn) {
-  		      handle.apply(self, [method].concat(args));
-  		    },
-  		    
-  		    widget: function (method, args, successFn, errorFn) {
-  		      handle.apply(self, [method].concat(args));
-  		    },
-  		    
   		    message: function (method, args, successFn, errorFn) {
   		      handle.apply(self, [method].concat(args));
   		    }
   		  },
   		  
   		  remote: {
-  		    dashboard: {},
-  		    
-  		    widget: {},
-  		    
   		    message: {}
   		  }
   		}
@@ -411,7 +115,7 @@
   			// Approach #1
   			// Must send an empty array as argument
   			// to match signature of message(method, args)
-  			rpc.dashboard(method, []);
+  			rpc.message(method, []);
   		} else {
   			// Check if the last argument is an object
   			// `success` and `error` functions.
@@ -424,10 +128,10 @@
   				var error = lastArg.error || function() {};
 
   				// Approach #3 and #4
-  				rpc.dashboard(method, args, success, error);
+  				rpc.message(method, args, success, error);
   			} else {
   				// Approach #2
-  				rpc.dashboard(method, args);	
+  				rpc.message(method, args);	
   			}
   		}
 
@@ -491,4 +195,4 @@
     return innertubeInstance;
   };
   
-}(window, _, jQuery));
+}(window, _, easyXDM));
